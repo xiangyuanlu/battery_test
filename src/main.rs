71099f10debd8bee2de67e9f41e12088b6035cbc
lxy <lxy@xbrother.com>
2024-01-09 19:39:37 +0800

    feat: modify serial port

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,46 @@ pub mod serial_port;
 pub mod webserver;
 use crate::webserver::start_web;
 
-pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-    let (a, b) = (matrix.len(), matrix[0].len());
-
-    let mut x = 0 as usize;
-    let mut y = b - 1;
-
-    while x < a && y >= 0 {
-        if target == matrix[x][y] {
-            return true;
-        } else if matrix[x][y] > target {
-            y -= 1;
-        } else {
-            if y + 1 != b {
-                break;
+//nums = [5,7,7,8,8,10], target = 8
+//nums = [5,7,7,8,8,10], target = 6
+pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    pub fn search_pos(nums: &Vec<i32>, target: i32, dir_left: bool) -> i32 {
+        let mut ans: i32 = 0;
+        let (mut l, mut r) = (0, nums.len() - 1);
+        while l <= r {
+            let m = (l + r) / 2;
+            if nums[m] > target || (dir_left && (nums[m] >= target)) {
+                r = m - 1;
+                ans = m as i32;
+            } else {
+                l = m + 1;
             }
-            x += 1;
         }
+
+        ans
     }
-    return false;
+
+    let l = search_pos(&nums, target, true);
+    let r = search_pos(&nums, target, false);
+    let mut ans = Vec::new();
+    if l <= r && r < nums.len() as i32 && nums[l as usize] == target && nums[r as usize] == target {
+        ans.push(l);
+        ans.push(r);
+    } else {
+        ans.push(-1);
+        ans.push(-1);
+    }
+    ans
 }
+
 #[tokio::main]
 async fn main() {
-    log::init_log();
-    start_web().await;
-    info!("Starting");
+    let v = [5, 7, 7, 8, 8, 10];
+    let t = 8;
+    let ans = search_range(v.to_vec(), t);
+    println!("{:?}", ans);
+
+    // log::init_log();
+    // start_web().await;
+    // info!("Starting");
 }
