65eca592e37468fbe8b246fa5e160ae03e00f71f
lxy <lxy@xbrother.com>
2024-01-10 09:45:30 +0800

    feat: use dyn SerialPort

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,11 @@ use crate::webserver::start_web;
 //nums = [5,7,7,8,8,10], target = 6
 pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
     pub fn search_pos(nums: &Vec<i32>, target: i32, dir_left: bool) -> i32 {
-        let mut ans: i32 = 0;
-        let (mut l, mut r) = (0, nums.len() - 1);
+        let mut ans: i32 = nums.len() as i32;
+        let (mut l, mut r) = (0 as i32, nums.len() as i32 - 1);
         while l <= r {
             let m = (l + r) / 2;
-            if nums[m] > target || (dir_left && (nums[m] >= target)) {
+            if nums[m as usize] > target || (dir_left && (nums[m as usize] >= target)) {
                 r = m - 1;
                 ans = m as i32;
             } else {
@@ -26,7 +26,7 @@ pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
     }
 
     let l = search_pos(&nums, target, true);
-    let r = search_pos(&nums, target, false);
+    let r = search_pos(&nums, target, false) - 1;
     let mut ans = Vec::new();
     if l <= r && r < nums.len() as i32 && nums[l as usize] == target && nums[r as usize] == target {
         ans.push(l);
@@ -40,8 +40,8 @@ pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
 
 #[tokio::main]
 async fn main() {
-    let v = [5, 7, 7, 8, 8, 10];
-    let t = 8;
+    let v = [1];
+    let t = 1;
     let ans = search_range(v.to_vec(), t);
     println!("{:?}", ans);
 
--- src/serial_port/port_manager.rs
+++ src/serial_port/port_manager.rs
@@ -1,8 +1,8 @@
-use std::{error, sync::Arc};
+use std::{error, rc::Rc, sync::Arc};
 
 use super::error::SerialPortError;
 use once_cell::sync::OnceCell;
-use serialport::{DataBits, FlowControl, Parity, SerialPort, StopBits};
+use serialport::{DataBits, FlowControl, Parity, SerialPort, StopBits, TTYPort};
 
 use anyhow::{Ok, Result};
 use dashmap::DashMap;
@@ -11,7 +11,7 @@ use parking_lot::Mutex;
 pub struct Port {
     name: String,
     baud: u32,
-    port: Box<dyn SerialPort>,
+    port: Box<dyn SerialPort + Sync>,
 }
 
 pub fn get_manager() -> &'static PortManager {
@@ -24,11 +24,11 @@ pub fn get_manager() -> &'static PortManager {
     })
 }
 pub struct PortManager {
-    ports: DashMap<String, Arc<Mutex<Port>>>,
+    ports: DashMap<String, Arc<Port>>,
 }
 
 impl PortManager {
-    pub fn get_port(&self, name: &str, rate: u32) -> Result<Arc<Mutex<Port>>> {
+    pub fn get_port(&self, name: &str, rate: u32) -> Result<Arc<Port>> {
         let dst_port = self.ports.get(name);
         match dst_port {
             Some(port) => {
@@ -36,12 +36,14 @@ impl PortManager {
                 return Ok(pt.clone());
             }
             None => {
-                if let core::result::Result::Ok(port) = serialport::new(name, rate).open() {
-                    let arc_port = Arc::new(Mutex::new(Port {
-                        name: name.to_string(),
-                        baud: rate,
-                        port: port,
-                    }));
+                if let core::result::Result::Ok(port) = serialport::new(name, rate).open_native() {
+                    let arc_port = Arc::new(
+                        (Port {
+                            name: name.to_string(),
+                            baud: rate,
+                            port: Box::new(port),
+                        }),
+                    );
                     self.ports.insert(name.to_string(), arc_port.clone());
                     return Ok(arc_port);
                 } else {
