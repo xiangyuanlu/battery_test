4c5c4e054619ae6fec5a7f2db2be8772dc19173f
lxy <lxy@xbrother.com>
2024-01-10 11:29:49 +0800

    feat: port add mutex

--- src/biz/mod.rs
+++ src/biz/mod.rs
@@ -1,12 +1,23 @@
 pub mod error;
-use crate::serial_port::*;
+use crate::serial_port::{port_manager::Port, *};
 use anyhow::{Ok, Result};
-use std::{thread, thread::JoinHandle};
+use parking_lot::Mutex;
+use std::{sync::Arc, thread, thread::JoinHandle};
+
+pub fn protocol_trans(mut port: Arc<Mutex<Port>>) {
+    let mut serial_buf: Vec<u8> = vec![0; 256];
+
+    loop {
+        let pt = port.lock().read(&mut serial_buf);
+        // let rt = port.read(&mut serial_buf);
+    }
+}
 
 pub fn workloop() -> Result<bool> {
     let port = port_manager::get_manager().get_port("dev/ttyS2", 9600)?;
     let thread_recv = thread::spawn({
         move || {
+
             // Channel::recv_loop(reqs_to_recv, port2, working_param_2);
         }
     });
--- src/serial_port/port_manager.rs
+++ src/serial_port/port_manager.rs
@@ -14,6 +14,19 @@ pub struct Port {
     port: Box<dyn SerialPort + Sync>,
 }
 
+impl Port {
+    pub fn read(&mut self, buf: &mut Vec<u8>) -> Result<u32> {
+        let t = self.port.read(buf)?;
+        Ok(t as u32)
+    }
+    pub fn get_name(&self) -> String {
+        self.name.to_owned()
+    }
+    pub fn get_baud(&self) -> u32 {
+        self.baud
+    }
+}
+
 pub fn get_manager() -> &'static PortManager {
     static INSTANCE: OnceCell<PortManager> = OnceCell::new();
     INSTANCE.get_or_init(|| {
@@ -24,11 +37,11 @@ pub fn get_manager() -> &'static PortManager {
     })
 }
 pub struct PortManager {
-    ports: DashMap<String, Arc<Port>>,
+    ports: DashMap<String, Arc<Mutex<Port>>>,
 }
 
 impl PortManager {
-    pub fn get_port(&self, name: &str, rate: u32) -> Result<Arc<Port>> {
+    pub fn get_port(&self, name: &str, rate: u32) -> Result<Arc<Mutex<Port>>> {
         let dst_port = self.ports.get(name);
         match dst_port {
             Some(port) => {
@@ -37,13 +50,11 @@ impl PortManager {
             }
             None => {
                 if let core::result::Result::Ok(port) = serialport::new(name, rate).open_native() {
-                    let arc_port = Arc::new(
-                        (Port {
-                            name: name.to_string(),
-                            baud: rate,
-                            port: Box::new(port),
-                        }),
-                    );
+                    let arc_port = Arc::new(Mutex::new(Port {
+                        name: name.to_string(),
+                        baud: rate,
+                        port: Box::new(port),
+                    }));
                     self.ports.insert(name.to_string(), arc_port.clone());
                     return Ok(arc_port);
                 } else {
