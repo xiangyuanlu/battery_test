ab3ca5995f106ab8d93a58151556607cbc7f5e2a
lxy <lxy@xbrother.com>
2024-01-12 14:52:37 +0800

    feat: modify port new fn

--- src/serial_port/port_manager.rs
+++ src/serial_port/port_manager.rs
@@ -1,4 +1,12 @@
-use std::{error, rc::Rc, sync::Arc};
+use std::sync::atomic::AtomicBool;
+use std::thread::JoinHandle;
+use std::time::Duration;
+use std::{
+    error,
+    rc::Rc,
+    sync::Arc,
+    thread::{self, Thread},
+};
 
 use super::error::SerialPortError;
 use once_cell::sync::OnceCell;
@@ -7,27 +15,88 @@ use serialport::{DataBits, FlowControl, Parity, SerialPort, StopBits, TTYPort};
 use anyhow::{Ok, Result};
 use dashmap::DashMap;
 use parking_lot::Mutex;
+use tracing::debug;
 
 pub struct Port {
     name: String,
     baud: u32,
-    port: Box<dyn SerialPort + Sync>,
+    tx: crossbeam::channel::Sender<DataFrame>,
+    rx: crossbeam::channel::Receiver<DataFrame>,
+    // port: Box<dyn SerialPort + Sync>,
+    trd: Option<JoinHandle<()>>,
+
+    runing: Arc<AtomicBool>,
+}
+
+#[derive(Debug)]
+pub struct DataFrame {
+    buf: Vec<u8>,
+}
+
+impl Drop for Port {
+    fn drop(&mut self) {
+        self.runing
+            .store(false, std::sync::atomic::Ordering::SeqCst);
+        if let Some(trd) = self.trd.take() {
+            trd.join().unwrap();
+        }
+    }
 }
 
 impl Port {
-    pub fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        let tt = self.port.read(buf);
-        if let Err(e) = tt {
-            print!("Error reading:{}", e.kind());
-        }
-        let rt = self.port.read(buf)?;
-        Ok(rt)
-    }
+    pub fn new(name: String, baud: u32, tout: u64) -> Result<Self> {
+        let (tx, rx) = crossbeam::channel::bounded(12);
 
-    pub fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        let rt = self.port.write(buf)?;
-        Ok(rt)
+        let mut port = serialport::new(name, baud)
+            .timeout(Duration::from_millis(tout))
+            .open_native()?;
+
+        let runing = Arc::new(AtomicBool::new(true));
+        let trd = {
+            let mut port = port.try_clone()?;
+            let runing = runing.clone();
+            let mut buffer = [0u8; 512];
+
+            thread::spawn(move || loop {
+                match port.read(&mut buffer[..]) {
+                    Result::Ok(size) => {
+                        debug!("{} rx:{:02X?}", name, &buffer[..size]);
+
+                        let df = DataFrame {
+                            buf: buffer.to_vec(),
+                        };
+                        tx.send(df);
+                    }
+                    Err(err) => {}
+                }
+            })
+        };
+        let port = Port {
+            name: "".to_string(),
+            baud: 1200,
+            tx,
+            rx,
+            trd: Some(trd),
+            runing,
+        };
+        Ok(port)
     }
+}
+
+impl Port {
+    // pub fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+    //     let tt = self.port.read(buf);
+    //     if let Err(e) = tt {
+    //         print!("Error reading:{}", e.kind());
+    //     }
+    //     let rt = self.port.read(buf)?;
+    //     Ok(rt)
+    // }
+
+    // pub fn write(&mut self, buf: &[u8]) -> Result<usize> {
+    //     let rt = self.port.write(buf)?;
+    //     Ok(rt)
+    // }
 
     pub fn get_name(&self) -> String {
         self.name.to_owned()
@@ -52,27 +121,29 @@ pub struct PortManager {
 
 impl PortManager {
     pub fn get_port(&self, name: &str, rate: u32) -> Result<Arc<Mutex<Port>>> {
-        let dst_port = self.ports.get(name);
-        match dst_port {
-            Some(port) => {
-                let pt = port.value();
-                return Ok(pt.clone());
-            }
-            None => {
-                if let core::result::Result::Ok(port) = serialport::new(name, rate).open_native() {
-                    let arc_port = Arc::new(Mutex::new(Port {
-                        name: name.to_string(),
-                        baud: rate,
-                        port: Box::new(port),
-                    }));
-                    self.ports.insert(name.to_string(), arc_port.clone());
-                    return Ok(arc_port);
-                } else {
-                    return Err(
-                        SerialPortError::SerialPortOpenFailed(name.to_string(), rate).into(),
-                    );
-                }
-            }
-        }
+        return Err(SerialPortError::SerialPortOpenFailed(name.to_string(), rate).into());
     }
+    //     let dst_port = self.ports.get(name);
+    //     match dst_port {
+    //         Some(port) => {
+    //             let pt = port.value();
+    //             return Ok(pt.clone());
+    //         }
+    //         None => {
+    //             if let core::result::Result::Ok(port) = serialport::new(name, rate).open_native() {
+    //                 let arc_port = Arc::new(Mutex::new(Port {
+    //                     name: name.to_string(),
+    //                     baud: rate,
+    //                     port: Box::new(port),
+    //                 }));
+    //                 self.ports.insert(name.to_string(), arc_port.clone());
+    //                 return Ok(arc_port);
+    //             } else {
+    //                 return Err(
+    //                     SerialPortError::SerialPortOpenFailed(name.to_string(), rate).into(),
+    //                 );
+    //             }
+    //         }
+    //     }
+    // }
 }
